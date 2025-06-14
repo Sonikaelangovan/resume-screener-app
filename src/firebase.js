57dc349606b79bc49387074109af79ea5ee09d3a
Sonikaelangovan <2403717620522306@cit.edu.in>
2025-06-14 23:24:15 +0530

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,18 @@
 
+// lib/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2Bj9fjndCrDIR2dmONdFWMf8GCOXEwGM",
+  authDomain: "resume-screener-app-14cf3.firebaseapp.com",
+  projectId: "resume-screener-app-14cf3",
+  storageBucket: "resume-screener-app-14cf3.firebasestorage.app",
+  messagingSenderId: "269980630639",
+  appId: "1:269980630639:web:a9bfe5f2f6101a03b2179a"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
